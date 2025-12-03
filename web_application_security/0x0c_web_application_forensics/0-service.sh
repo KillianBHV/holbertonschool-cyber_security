8ed70c279af3ef81b4781d770c417079a9d3db1a
Killian BEHAVA <11841@holbertonstudents.com>
2025-12-03 08:21:04 -0500

    NMAP Scripting

--- web_application_security/0x0c_web_application_forensics/0-service.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-awk '{print $6}' auth.log | sort | uniq -c | sort -nr
-
--- web_application_security/0x0c_web_application_forensics/3-ips.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-grep "Accepted password for root" auth.log | grep -Po 'from \K[\d.]+' | sort -u | wc -l
-
