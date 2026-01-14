bb56827e57fbdf7bfbb24bde468a4c660a0d505a
Killian BEHAVA <11841@holbertonstudents.com>
2026-01-14 04:29:02 -0500

    RUBY Scripting - Part 1

---
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -0,0 +1,13 @@
+require 'json'
+
+def merge_json_files(file1_path, file2_path)
+  file1 = JSON.parse(File.read(file1_path))
+  file2 = JSON.parse(File.read(file2_path))
+  merged = file2 + file1
+
+  File.open(file2_path, 'w') do |file|
+    file.write(JSON.pretty_generate(file))
+  end
+
+  puts 'File merged successfully'
+end
