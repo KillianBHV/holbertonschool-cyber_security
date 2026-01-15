89ee0eea5fc40b534c55581e99db7ef743285a82
Killian BEHAVA <11841@holbertonstudents.com>
2026-01-15 09:05:08 -0500

    RUBY Scripting - Update

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -2,13 +2,15 @@
 require 'json'
 
 def merge_json_files(file1_path, file2_path)
-  file1 = JSON.parse(File.read(file1_path))
-  file2 = JSON.parse(File.read(file2_path))
-  merged = file2 + file1
+  file1 = File.read(file1_path)
+  data_file1 = JSON.parse(file1)
 
-  File.open(file2_path, 'w') do |file|
-    file.write(JSON.pretty_generate(file))
-  end
+  file2 = File.read(file2_path)
+  data_file2 = JSON.parse(file2)
 
-  puts 'File merged successfully'
+  merged = data_file2 + data_file1
+
+  File.write(file2_path, JSON.pretty_generate(merged))
+
+  puts 'Merged JSON successful!'
 end
