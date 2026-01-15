1bdb2428a6ba7d5c2a77adba09deff508a6e4f77
Killian BEHAVA <11841@holbertonstudents.com>
2026-01-15 09:16:46 -0500

    RUBY Scripting - Update

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -3,23 +3,26 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-def post_request(url, body_params)
-  uri = URI(url)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = (uri.scheme == 'https')
+def post_request(url, body_params = {})
+  uri = URI.parse(url)
 
+  request = Net::HTTP::Post.new(uri)
+  request.content_type = 'application/json'
+  request.body = body_params.to_json unless body_params.empty?
 
-  req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
-  req.body = body_params.to_json
-  response = http.request(req)
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    http.request(request)
+  end
 
-  puts "Response status: #{response.code} #{response.message}"
-  puts "\nResponse body:"
-
-  begin
-    json_data = JSON.parse(response.body)
-    puts JSON.pretty_generate(json_data)
-  rescue JSON::ParseError
-    puts response.body
+  status_code = response.code
+  status_message = response.message
+  puts "Response status: #{status_code} #{status_message}"
+  
+  if response.code.to_i >= 200 && response.code.to_i < 300
+    puts "Response body:"
+    data = JSON.parse(response.body)
+    puts JSON.pretty_generate(data)
+  else
+    puts "Failed to post data"
   end
 end
