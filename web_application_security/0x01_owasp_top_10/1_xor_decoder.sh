13c450412b4351de0d4ff4cd49cf6a4eee5c205c
Killian BEHAVA <11841@holbertonstudents.com>
2026-01-22 05:04:25 -0500

    UPDATE Script Task 1

--- web_application_security/0x01_owasp_top_10/1_xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1_xor_decoder.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-base64_decode=$(echo "$1" | sed -e 's/{xor}//g' | base64 --decode | tr -d \\n | od -An -tx1 | tr -dc '[[:alnum:]]')
-size=${#base64_decode}
+decoding=$(echo $1 | sed -e "s/{xor}//g" | base64 --decode)
+b64_clean=$(echo "$decoding" | tr -d \\n | od -An -tx1 | tr -dc '[[:alnum:]]')
+size=${#b64_clean}
 for (( i=0; i<size; i+=2 )); do
-	hexa_xor_underscore=$((16#${base64_decode:i:2} ^ 95))
-	result+=$(printf -v hex "%x" "$hexa_xor_underscore" && echo "\x$hex")
+	hexa_xor_underscore=$((16#${b64_clean:i:2} ^ 95))
+	result+=$(printf '\\x%x' $hexa_xor_underscore)
 done
-
 echo -e $result
