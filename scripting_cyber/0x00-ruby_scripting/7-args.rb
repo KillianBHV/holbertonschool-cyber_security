9868532e4cb83855260de93b7a0e9a157d380a1a
Killian BEHAVA <11841@holbertonstudents.com>
2026-01-22 05:04:26 -0500

    Scripting Cyber - Part 2

---
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+def print_arguments
+  if ARGV.empty?
+    puts "No arguments provided."
+  else
+    ARGV.each.with_index(1) do |arg, index|
+      puts "#{index}. #{arg}"
+    end
+  end
+end
---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'uri'
+require 'json'
+
+def post_request(url, body_params)
+  uri = URI(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = (uri.scheme == 'https')
+
+
+  req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+  req.body = body_params.to_json
+  response = http.request(req)
+
+  puts "Response status: #{response.code} #{response.message}"
+  puts "\nResponse body:"
+
+  begin
+    json_data = JSON.parse(response.body)
+    puts JSON.pretty_generate(json_data)
+  rescue JSON::ParseError
+    puts response.body
+  end
+end
